e8acea6232b08fe1396fc90b4999836c468e9415
r3qin <rq2167@columbia.edu>
2024-07-17 02:29:01 +0800

    Updated nvim coc

--- config/nvim/lua/plugins/coc.lua
+++ config/nvim/lua/plugins/coc.lua
@@ -1,4 +1,7 @@
 return {
   'neoclide/coc.nvim',
-  branch = 'release'
+  branch = 'release',
+  keys = {
+    { "<CR>", 'coc#pum#visible() ? coc#pum#confirm() : "<C-g>u<CR><c-r>=coc#on_enter()<CR>"', mode = 'i', noremap = true, silent = true, expr = true }
+  }
 }
